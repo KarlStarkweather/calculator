017c4faf46a2c4637a22aa30b9b66483032c3fd6
KarlStarkweather <stark13@usa.net>
2021-10-26 19:05:36 -0700

    tested all new functions

--- js/scripts.js
+++ js/scripts.js
@@ -18,4 +18,4 @@ function divide(number1, number2) {
 const number1 = parseInt(prompt("Enter a number: "));
 const number2 = parseInt(prompt("Enter another number: "));
 
-alert(add(number1, number2));
+alert(divide(number1, number2));
